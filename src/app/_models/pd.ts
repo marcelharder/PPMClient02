7872b0a6a7ab1c2a29f4e98bda8d7d87bd94c74c
Marcel harder <marcelharder@protonmail.com>
2025-09-03 12:04:31 +0200

    after toaster working

---
+++ src/app/_models/pd.ts
@@ -0,0 +1,6 @@
+export interface PatientData {
+    Age: number;        // years            
+    Gender: string;     // "M" or "F"
+    Height: number;     // meters
+    Weight: number;     // kg
+}
--- src/app/patient-data/patient-data.component.ts
+++ src/app/patient-data/patient-data.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-patient-data',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './patient-data.component.html',
   styleUrl: './patient-data.component.css'
 })
 export class PatientDataComponent {
+constructor(private toastr: ToastrService) {}
+onSubmit() {
+this.toastr.success('Data submitted successfully!', 'Success');
+}
+  pd: any = { Age: 1, Gender: "M", Height: 0.0, Weight: 0.0}
 
 }
