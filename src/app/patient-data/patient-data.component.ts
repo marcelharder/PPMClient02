3bf24c815cce540bcd2f9eff3176c0fe44a8e626
Marcel harder <marcelharder@protonmail.com>
2025-09-03 10:55:17 +0200

    initial commit

---
+++ src/app/patient-data/patient-data.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-patient-data',
+  standalone: true,
+  imports: [],
+  templateUrl: './patient-data.component.html',
+  styleUrl: './patient-data.component.css'
+})
+export class PatientDataComponent {
+
+}
