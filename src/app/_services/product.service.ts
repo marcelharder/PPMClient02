e8f0b7f528e15e85edc75b8cee6deca37c64cdb4
m_harder88@yahoo.com <m_harder88@yahoo.com>
2025-09-12 16:13:35 +0200

    busy with valvesizes

--- src/app/_services/product.service.ts
+++ src/app/_services/product.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../environments/environment.development';
 import { TypeOfValve } from '../_models/TypeOfValve';
+import { valveSize } from '../_models/valveSize';
 
 
 @Injectable({
@@ -12,11 +13,16 @@ export class ProductService {
     private baseUrl = environment.apiUrl;
     http = inject(HttpClient);
     valveList = signal<TypeOfValve[]>([]);
+    getSelectedValveTypeId = signal(0);
+    valveSizeList= signal<valveSize[]>([]);
   
     
 
     getProductsByVTP(v:number,t:string,p:string): Observable<TypeOfValve[]> 
     { return this.http.get<TypeOfValve[]>(this.baseUrl + 'productsByVTP/' + v + '/' + t + '/' + p);
+    }
+    getValveSizes(): Observable<valveSize[]>{
+        return this.http.get<valveSize[]>(this.baseUrl + 'valveSizes');
     }
 
 
---
+++ src/app/valve-sizes/valve-sizes.component.ts
@@ -0,0 +1,24 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { ProductService } from '../_services/product.service';
+
+@Component({
+  selector: 'app-valve-sizes',
+  standalone: true,
+  imports: [],
+  templateUrl: './valve-sizes.component.html',
+  styleUrl: './valve-sizes.component.css'
+})
+export class ValveSizesComponent implements OnInit  {
+  constructor(private toastr: ToastrService) { }
+
+  router = inject(Router);
+  proc = inject(ProductService);
+  selectedValveTypeId = 0;
+
+    ngOnInit(): void {
+    this.selectedValveTypeId = this.proc.getSelectedValveTypeId();
+    // get the sizes for the selected valve type
+  }
+}
