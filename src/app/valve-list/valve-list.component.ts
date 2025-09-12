a6aa86d3f8853122191d0e1dd6313f861642847f
m_harder88@yahoo.com <m_harder88@yahoo.com>
2025-09-12 15:54:18 +0200

    valvelist works now

--- src/app/valve-list/valve-list.component.ts
+++ src/app/valve-list/valve-list.component.ts
@@ -2,25 +2,38 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { TypeOfValve } from '../_models/TypeOfValve';
 import { ProductService } from '../_services/product.service';
+import { NgFor } from '@angular/common';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-valve-list',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, NgFor],
   templateUrl: './valve-list.component.html',
   styleUrl: './valve-list.component.css'
 })
 export class ValveListComponent implements OnInit   {
-  vl = signal<TypeOfValve[]>([]);
+  constructor(private toastr: ToastrService) { }
+  selectedValve: TypeOfValve = {} as TypeOfValve;
+  valvel = signal<TypeOfValve[]>([]);
   proc = inject(ProductService);
+   router = inject(Router);
   
   ngOnInit(): void {
   
-    this.vl = this.proc.valveList;
-    console.log(this.vl());
+    this.valvel = this.proc.valveList;
+    console.log(this.valvel());
   }
   
   onSubmit() {}
+
+  Cancel(){this.router.navigate(['/valve-data'])}
+
+  getDetails(v: TypeOfValve) {
+
+     this.toastr.error('this valve selected ...', 'Error');
+  }
 
 }
 
